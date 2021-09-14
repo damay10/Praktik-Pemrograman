d569d63167eb91f4ce60ae86f1b30f24fe9fced5
Damayanti Wahyu M <angelinamahesti1@gmail.com>
2021-09-14 15:23:22 +0700

    Pertemuan 3

--- Pertemuan 2/Hello.java
+++ Pertemuan 2/Hello.java
@@ -1,3 +1,7 @@
+/*
+NIM : 21537144020
+Nama : Damayanti Wahyu Mahesti
+*/
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
---
+++ Pertemuan 3/CobaVar.java
@@ -0,0 +1,10 @@
+public class CobaVar {
+    public static void main(String[] args) {
+        String namaDepan = "Damayanti";
+        int umur = 18;
+        double tinggiBadan = 150;
+        System.out.println("Nama Saya " + namaDepan);
+        System.out.println("Umur saya " + umur + " tahun");
+        System.out.println("Tinggi badan saya " + (int) tinggiBadan + " cm");
+    }
+}
