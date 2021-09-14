fae8b40cb1e21530673432f2a9a2eb4c1d240757
Damayanti Wahyu M <angelinamahesti1@gmail.com>
2021-09-14 15:30:21 +0700

    commit

---
+++ Pertemuan 3/Identitas.java
@@ -0,0 +1,23 @@
+public class Identitas {
+    public static void main(String[] args) {
+        String namaDepan = "Damayanti";
+        String namaTengah = "Wahyu";
+        String namaBelakang = "Mahesti";
+        String namaPanggilan = "Damay";
+        String nim = "21537144020";
+        int angkatan = 2021;
+        String programStudi = "Teknologi Informasi";
+        String jurusan = "Pendidikan Teknik Elektronika dan Informatika";
+        String fakultas = "Teknik";
+        double ipk = 3.5;
+        System.out.println("Identitas saya sebagai mahasiswa UNY");
+        System.out.println("Nama saya " + namaDepan + " " + namaTengah + " " + namaBelakang + " biasa dipanggil " + namaPanggilan);
+        System.out.println("NIM saya " + nim);
+        System.out.println("Saya angkatan tahun " + angkatan);
+        System.out.println("Saya dari program studi " + programStudi);
+        System.out.println("Jurusan saya adalah " + jurusan);
+        System.out.println("Fakultas saya adalah " + fakultas);
+        System.out.println("Harapan IPK saya semester ini minimal " + ipk);
+    }
+    
+}
---
+++ Pertemuan 3/identitas.py
@@ -0,0 +1,18 @@
+namaDepan = "Damayanti"
+namaTengah = "Wahyu"
+namaBelakang = "Mahesti"
+namaPanggilan = "Damay"
+nim = 21537144020
+angkatan = 2021
+programStudi = "Teknologi Informasi"
+jurusan = "Pendidikan Teknik Elektronika dan Informatika"
+fakultas = "Teknik"
+ipk = 3.5
+print("Identitas pribadi saya sebagai mahasiswa UNY")
+print("Nama saya adalah " + namaDepan + " " + namaTengah + " " + namaBelakang + " biasa dipanggil " + namaPanggilan)
+print("NIM saya " + str(nim))
+print("Saya angkatan tahun " + str(angkatan))
+print("Saya dari program studi " + programStudi)
+print("Jurusan saya adalah " + jurusan)
+print("Fakultas saya adalah " + fakultas)
+print("Harapan IPK saya semseter ini minimal " + str(ipk))
