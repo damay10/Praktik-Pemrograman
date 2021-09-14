96dd0d4727077d4248828532dd3d133c6e953e69
Damayanti Wahyu Mahesti <90198868+damay10@users.noreply.github.com>
2021-09-14 14:08:55 +0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+/*
+NIM : 21537144020
+Nama : Damayanti Wahyu Mahesti
+*/
+public class Hello {
+    public static void main(String[] args) {
+        //untuk menampilkan teks
+        System.out.println("Hello world");
+    }
+}
---
+++ hello.py
@@ -0,0 +1,9 @@
+namaDepan = "Damayanti"
+umur = 18
+tinggiBadan = 150
+print("Nama saya adalah " + namaDepan)
+print("Umur saya adalah " + str(umur) + " tahun")
+print("Tinggi badan saya " + str(tinggiBadan) + " cm")
+print(type(namaDepan))
+print(type(umur))
+print(type(tinggiBadan))
